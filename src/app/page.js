15403f7cbd44ed912b6f86ed8527d6d1a1cf5bc3
ishaan-jaff <ishaanjaffer0324@gmail.com>
2023-09-20 15:46:30 -0700

    put window in useEffect

--- src/app/page.js
+++ src/app/page.js
@@ -1,8 +1,8 @@
 'use client'
-// pages/index.js
-import React from 'react';
+import { React, useEffect, useState } from 'react';
 import { Alert } from 'antd';
 import { Card, Text, Title, Grid, Col, Metric, Italic, Callout, Badge } from "@tremor/react";
+
 const keyToTitle = {
   "PRE_CALL": "Arguments passed to Provider",
   "EXCEPTION": "Exception",
@@ -14,25 +14,35 @@ const topRowKeys = ['PRE_CALL', 'EXCEPTION'];
 const bottomRowKeys = ['KWARGS', 'POST_CALL'];
 
 const HomePage = () => {
+  const [jsonData, setJsonData] = useState(null);
+  const [exception, setException] = useState('');
+  const [model, setModel] = useState('');
+  const [messages, setMessages] = useState('');
 
-  // Get the current URL
-  var currentURL = window.location.href;
-  console.log('current url', currentURL);
+  useEffect(() => {
+    // Get the current URL
+    var currentURL = window.location.href;
+    console.log('current url', currentURL);
 
-  let params = (new URL(currentURL)).searchParams;
-  console.log('params', params);
-  let jsonData = params.get('data'); // is the string with your JSON
-  console.log("json data", jsonData);
-  try {
-      jsonData = JSON.parse(jsonData);
-  } catch (error) {
-      console.error("Error parsing JSON data:", error);
+    let params = (new URL(currentURL)).searchParams;
+    console.log('params', params);
+    const jsonDataString = params.get('data'); // is the string with your JSON
+    console.log("json data", jsonDataString);
+    try {
+        const parsedData = JSON.parse(jsonDataString);
+        setJsonData(parsedData);
+        setException(parsedData['EXCEPTION']);
+        setModel(parsedData['KWARGS']['model']);
+        setMessages(parsedData['KWARGS']['messages']);
+    } catch (error) {
+        console.error("Error parsing JSON data:", error);
+    }
+  }, []); // Empty dependency array to run this effect only once after component mount
+
+  // Render the component only when jsonData is available
+  if (!jsonData) {
+    return null; // or loading indicator
   }
-
-  let exception = jsonData['EXCEPTION']
-  console.log(exception)
-  let model = jsonData['KWARGS']['model']
-  let messages = jsonData['KWARGS']['messages']
 
   const topRowCards = topRowKeys.map((key) => (
     <Col numColSpan={1} key={key} className='mt-5 mx-2'>
@@ -50,7 +60,6 @@ const HomePage = () => {
       </Card>
     </Col>
   ));
-
 
   const bottomRowCards = bottomRowKeys.map((key) => (
     <Col numColSpan={1} key={key} className='mt-5 mx-2'>
@@ -75,25 +84,23 @@ const HomePage = () => {
   return (
     <div>
       <div style={pageStyle}>
-      <Title>LiteLLM Request Log</Title>
-      <Metric className='my-4' color="sky">litellm.completion() <Badge color='red'>error</Badge></Metric>
-      <Grid numItems={2}>
-        <Col className='mx-2 mt-3'>
-          <Card>
-            <Title>Model: {model}</Title>
-            <Title>Messages: {messages}</Title>
-          </Card>
-        </Col>
-        <Col className='mx-2'>
-          <Alert description={exception} type="error" showIcon className='mx-2 mt-5'/>
-        </Col>
-        {topRowCards}
-        {bottomRowCards}
-      </Grid>
+        <Title>LiteLLM Request Log</Title>
+        <Metric className='my-4' color="sky">litellm.completion() <Badge color='red'>error</Badge></Metric>
+        <Grid numItems={2}>
+          <Col className='mx-2 mt-3'>
+            <Card>
+              <Title>Model: {model}</Title>
+              <Title>Messages: {messages}</Title>
+            </Card>
+          </Col>
+          <Col className='mx-2'>
+            <Alert description={exception} type="error" showIcon className='mx-2 mt-5'/>
+          </Col>
+          {topRowCards}
+          {bottomRowCards}
+        </Grid>
+      </div>
     </div>
-
-    </div>
-    
   );
 };
 
